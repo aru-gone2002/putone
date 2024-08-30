1f3e4d6f00e45ab5a54ec0102fb928b417b9de00
nunupy345345 <airinota@outlook.jp>
2024-08-30 22:01:30 +0900

    sound clear

--- lib/view/module_page/post_detail_page.dart
+++ lib/view/module_page/post_detail_page.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:putone/local_database.dart';
+import 'package:just_audio/just_audio.dart';
 
-class PostDetailView extends StatelessWidget {
+class PostDetailView extends StatefulWidget {
   const PostDetailView({super.key, required this.post});
 
   final LocalUserPost post;
+
+  @override
+  State<PostDetailView> createState() => _PostDetailViewState();
+}
+
+class _PostDetailViewState extends State<PostDetailView> {
+  late AudioPlayer _audioPlayer;
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+    _initAudioPlayer();
+  }
+
+  Future<void> _initAudioPlayer() async {
+    try {
+      await _audioPlayer.setAudioSource(
+          AudioSource.uri(Uri.parse(widget.post.postMusicPreciewUrl)));
+    } catch (e) {
+      print("Error initializing audio player: $e");
+    }
+  }
+
+  void _playPause() {
+    if (_isPlaying) {
+      _audioPlayer.pause();
+      print("pause");
+    } else {
+      _audioPlayer.play();
+      print('play');
+    }
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext contest) {
@@ -16,12 +60,18 @@ class PostDetailView extends StatelessWidget {
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.network(post.postMusicImg),
+            Image.network(widget.post.postMusicImg),
             SizedBox(height: 20),
-            Text(post.postMusicName),
-            Text(post.postMusicArtistName),
+            Text(widget.post.postMusicName),
+            Text(widget.post.postMusicArtistName),
             SizedBox(height: 20),
-            Text(post.postMsg),
+            Text(widget.post.postMsg),
+            SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: _playPause,
+              icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+              label: Text(_isPlaying ? 'Pause' : 'Play'),
+            )
           ],
         )));
   }
