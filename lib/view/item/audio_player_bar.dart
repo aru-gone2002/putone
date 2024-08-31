3371aac32ebb18935356562cb3048926a5a2ef06
nunupy345345 <airinota@outlook.jp>
2024-09-01 08:20:05 +0900

    split to module

---
+++ lib/view/item/audio_player_bar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+class AudioPlayerBar extends StatelessWidget {
+  final AudioPlayer audioPlayer;
+  const AudioPlayerBar({Key? key, required this.audioPlayer}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        StreamBuilder<PlayerState>(
+          stream: audioPlayer.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            final processingState = playerState?.processingState;
+            final playing = playerState?.playing;
+            if (processingState == ProcessingState.loading ||
+                processingState == ProcessingState.buffering) {
+              return Container(
+                margin: EdgeInsets.all(8.0),
+                width: 32.0,
+                height: 32.0,
+                child: CircularProgressIndicator(),
+              );
+            } else if (playing != true) {
+              return IconButton(
+                icon: Icon(Icons.play_arrow),
+                iconSize: 32.0,
+                onPressed: audioPlayer.play,
+              );
+            } else if (processingState != ProcessingState.completed) {
+              return IconButton(
+                icon: Icon(Icons.pause),
+                iconSize: 32.0,
+                onPressed: audioPlayer.pause,
+              );
+            } else {
+              return IconButton(
+                icon: Icon(Icons.replay),
+                iconSize: 32.0,
+                onPressed: () => audioPlayer.seek(Duration.zero, index: 0),
+              );
+            }
+          },
+        ),
+        StreamBuilder<Duration>(
+            stream: audioPlayer.positionStream,
+            builder: (context, snapshot) {
+              final position = snapshot.data;
+              final duration = audioPlayer.duration;
+              if (position != null && duration != null) {
+                return Column(
+                  children: [
+                    LinearProgressIndicator(
+                      value: position.inMilliseconds / duration.inMilliseconds,
+                    ),
+                  ],
+                );
+              } else {
+                return SizedBox.shrink();
+              }
+            })
+      ],
+    );
+  }
+}
--- lib/view/module_page/post_detail_page.dart
+++ lib/view/module_page/post_detail_page.dart
@@ -26,23 +26,25 @@ class _PostDetailViewState extends State<PostDetailView> {
     try {
       await _audioPlayer.setAudioSource(
           AudioSource.uri(Uri.parse(widget.post.postMusicPreciewUrl)));
+      // widgetが開かれたら自動で再生する
+      _audioPlayer.play();
     } catch (e) {
       print("Error initializing audio player: $e");
     }
   }
 
-  void _playPause() {
-    if (_isPlaying) {
-      _audioPlayer.pause();
-      print("pause");
-    } else {
-      _audioPlayer.play();
-      print('play');
-    }
-    setState(() {
-      _isPlaying = !_isPlaying;
-    });
-  }
+  // void _playPause() {
+  //   if (_isPlaying) {
+  //     _audioPlayer.pause();
+  //     print("pause");
+  //   } else {
+  //     _audioPlayer.play();
+  //     print('play');
+  //   }
+  //   setState(() {
+  //     _isPlaying = !_isPlaying;
+  //   });
+  // }
 
   @override
   void dispose() {
@@ -67,11 +69,60 @@ class _PostDetailViewState extends State<PostDetailView> {
             SizedBox(height: 20),
             Text(widget.post.postMsg),
             SizedBox(height: 20),
-            ElevatedButton.icon(
-              onPressed: _playPause,
-              icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
-              label: Text(_isPlaying ? 'Pause' : 'Play'),
-            )
+            StreamBuilder<PlayerState>(
+              stream: _audioPlayer.playerStateStream,
+              builder: (context, snapshot) {
+                final playerState = snapshot.data;
+                final processingState = playerState?.processingState;
+                final playing = playerState?.playing;
+                if (processingState == ProcessingState.loading ||
+                    processingState == ProcessingState.buffering) {
+                  return Container(
+                    margin: EdgeInsets.all(8.0),
+                    width: 64.0,
+                    height: 64.0,
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (playing != true) {
+                  return IconButton(
+                    icon: Icon(Icons.play_arrow),
+                    iconSize: 64.0,
+                    onPressed: _audioPlayer.play,
+                  );
+                } else if (processingState != ProcessingState.completed) {
+                  return IconButton(
+                    icon: Icon(Icons.pause),
+                    iconSize: 64.0,
+                    onPressed: _audioPlayer.pause,
+                  );
+                } else {
+                  return IconButton(
+                    icon: Icon(Icons.replay),
+                    iconSize: 64.0,
+                    onPressed: () => _audioPlayer.seek(Duration.zero, index: 0),
+                  );
+                }
+              },
+            ),
+            StreamBuilder<Duration>(
+              stream: _audioPlayer.positionStream,
+              builder: (context, snapshot) {
+                final position = snapshot.data;
+                final duration = _audioPlayer.duration;
+                if (position != null && duration != null) {
+                  return LinearProgressIndicator(
+                    value: position.inMilliseconds / duration.inMilliseconds,
+                  );
+                } else {
+                  return SizedBox.shrink();
+                }
+              },
+            ),
+            // ElevatedButton.icon(
+            //   onPressed: _playPause,
+            //   icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+            //   label: Text(_isPlaying ? 'Pause' : 'Play'),
+            // )
           ],
         )));
   }
