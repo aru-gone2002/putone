a70cb4c318b0b773a77e7791e3cf1d29e3b95365
yugo yoshida <yugo.yoshida@gmail.com>
2024-10-27 08:34:24 +0900

    issue/98/prepare_for_items

---
+++ lib/data/item/item.freezed.dart
@@ -0,0 +1,428 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'item.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Item _$ItemFromJson(Map<String, dynamic> json) {
+  return _Item.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Item {
+  String get uid => throw _privateConstructorUsedError;
+  String get itemId => throw _privateConstructorUsedError;
+  bool get isSaled => throw _privateConstructorUsedError;
+  String get artistSpotifyId => throw _privateConstructorUsedError;
+  String get artistName => throw _privateConstructorUsedError;
+  String get artistSpotifyUrl => throw _privateConstructorUsedError;
+  List<String> get itemImgs => throw _privateConstructorUsedError;
+  String get itemName => throw _privateConstructorUsedError;
+  String get itemDescription => throw _privateConstructorUsedError;
+  int get price => throw _privateConstructorUsedError;
+  String get size => throw _privateConstructorUsedError;
+  String get categories => throw _privateConstructorUsedError;
+
+  /// Serializes this Item to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of Item
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $ItemCopyWith<Item> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ItemCopyWith<$Res> {
+  factory $ItemCopyWith(Item value, $Res Function(Item) then) =
+      _$ItemCopyWithImpl<$Res, Item>;
+  @useResult
+  $Res call(
+      {String uid,
+      String itemId,
+      bool isSaled,
+      String artistSpotifyId,
+      String artistName,
+      String artistSpotifyUrl,
+      List<String> itemImgs,
+      String itemName,
+      String itemDescription,
+      int price,
+      String size,
+      String categories});
+}
+
+/// @nodoc
+class _$ItemCopyWithImpl<$Res, $Val extends Item>
+    implements $ItemCopyWith<$Res> {
+  _$ItemCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of Item
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? uid = null,
+    Object? itemId = null,
+    Object? isSaled = null,
+    Object? artistSpotifyId = null,
+    Object? artistName = null,
+    Object? artistSpotifyUrl = null,
+    Object? itemImgs = null,
+    Object? itemName = null,
+    Object? itemDescription = null,
+    Object? price = null,
+    Object? size = null,
+    Object? categories = null,
+  }) {
+    return _then(_value.copyWith(
+      uid: null == uid
+          ? _value.uid
+          : uid // ignore: cast_nullable_to_non_nullable
+              as String,
+      itemId: null == itemId
+          ? _value.itemId
+          : itemId // ignore: cast_nullable_to_non_nullable
+              as String,
+      isSaled: null == isSaled
+          ? _value.isSaled
+          : isSaled // ignore: cast_nullable_to_non_nullable
+              as bool,
+      artistSpotifyId: null == artistSpotifyId
+          ? _value.artistSpotifyId
+          : artistSpotifyId // ignore: cast_nullable_to_non_nullable
+              as String,
+      artistName: null == artistName
+          ? _value.artistName
+          : artistName // ignore: cast_nullable_to_non_nullable
+              as String,
+      artistSpotifyUrl: null == artistSpotifyUrl
+          ? _value.artistSpotifyUrl
+          : artistSpotifyUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      itemImgs: null == itemImgs
+          ? _value.itemImgs
+          : itemImgs // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      itemName: null == itemName
+          ? _value.itemName
+          : itemName // ignore: cast_nullable_to_non_nullable
+              as String,
+      itemDescription: null == itemDescription
+          ? _value.itemDescription
+          : itemDescription // ignore: cast_nullable_to_non_nullable
+              as String,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as int,
+      size: null == size
+          ? _value.size
+          : size // ignore: cast_nullable_to_non_nullable
+              as String,
+      categories: null == categories
+          ? _value.categories
+          : categories // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ItemImplCopyWith<$Res> implements $ItemCopyWith<$Res> {
+  factory _$$ItemImplCopyWith(
+          _$ItemImpl value, $Res Function(_$ItemImpl) then) =
+      __$$ItemImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String uid,
+      String itemId,
+      bool isSaled,
+      String artistSpotifyId,
+      String artistName,
+      String artistSpotifyUrl,
+      List<String> itemImgs,
+      String itemName,
+      String itemDescription,
+      int price,
+      String size,
+      String categories});
+}
+
+/// @nodoc
+class __$$ItemImplCopyWithImpl<$Res>
+    extends _$ItemCopyWithImpl<$Res, _$ItemImpl>
+    implements _$$ItemImplCopyWith<$Res> {
+  __$$ItemImplCopyWithImpl(_$ItemImpl _value, $Res Function(_$ItemImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of Item
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? uid = null,
+    Object? itemId = null,
+    Object? isSaled = null,
+    Object? artistSpotifyId = null,
+    Object? artistName = null,
+    Object? artistSpotifyUrl = null,
+    Object? itemImgs = null,
+    Object? itemName = null,
+    Object? itemDescription = null,
+    Object? price = null,
+    Object? size = null,
+    Object? categories = null,
+  }) {
+    return _then(_$ItemImpl(
+      uid: null == uid
+          ? _value.uid
+          : uid // ignore: cast_nullable_to_non_nullable
+              as String,
+      itemId: null == itemId
+          ? _value.itemId
+          : itemId // ignore: cast_nullable_to_non_nullable
+              as String,
+      isSaled: null == isSaled
+          ? _value.isSaled
+          : isSaled // ignore: cast_nullable_to_non_nullable
+              as bool,
+      artistSpotifyId: null == artistSpotifyId
+          ? _value.artistSpotifyId
+          : artistSpotifyId // ignore: cast_nullable_to_non_nullable
+              as String,
+      artistName: null == artistName
+          ? _value.artistName
+          : artistName // ignore: cast_nullable_to_non_nullable
+              as String,
+      artistSpotifyUrl: null == artistSpotifyUrl
+          ? _value.artistSpotifyUrl
+          : artistSpotifyUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      itemImgs: null == itemImgs
+          ? _value._itemImgs
+          : itemImgs // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      itemName: null == itemName
+          ? _value.itemName
+          : itemName // ignore: cast_nullable_to_non_nullable
+              as String,
+      itemDescription: null == itemDescription
+          ? _value.itemDescription
+          : itemDescription // ignore: cast_nullable_to_non_nullable
+              as String,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as int,
+      size: null == size
+          ? _value.size
+          : size // ignore: cast_nullable_to_non_nullable
+              as String,
+      categories: null == categories
+          ? _value.categories
+          : categories // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ItemImpl with DiagnosticableTreeMixin implements _Item {
+  const _$ItemImpl(
+      {required this.uid,
+      required this.itemId,
+      required this.isSaled,
+      required this.artistSpotifyId,
+      required this.artistName,
+      required this.artistSpotifyUrl,
+      required final List<String> itemImgs,
+      required this.itemName,
+      required this.itemDescription,
+      required this.price,
+      required this.size,
+      required this.categories})
+      : _itemImgs = itemImgs;
+
+  factory _$ItemImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ItemImplFromJson(json);
+
+  @override
+  final String uid;
+  @override
+  final String itemId;
+  @override
+  final bool isSaled;
+  @override
+  final String artistSpotifyId;
+  @override
+  final String artistName;
+  @override
+  final String artistSpotifyUrl;
+  final List<String> _itemImgs;
+  @override
+  List<String> get itemImgs {
+    if (_itemImgs is EqualUnmodifiableListView) return _itemImgs;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_itemImgs);
+  }
+
+  @override
+  final String itemName;
+  @override
+  final String itemDescription;
+  @override
+  final int price;
+  @override
+  final String size;
+  @override
+  final String categories;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'Item(uid: $uid, itemId: $itemId, isSaled: $isSaled, artistSpotifyId: $artistSpotifyId, artistName: $artistName, artistSpotifyUrl: $artistSpotifyUrl, itemImgs: $itemImgs, itemName: $itemName, itemDescription: $itemDescription, price: $price, size: $size, categories: $categories)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'Item'))
+      ..add(DiagnosticsProperty('uid', uid))
+      ..add(DiagnosticsProperty('itemId', itemId))
+      ..add(DiagnosticsProperty('isSaled', isSaled))
+      ..add(DiagnosticsProperty('artistSpotifyId', artistSpotifyId))
+      ..add(DiagnosticsProperty('artistName', artistName))
+      ..add(DiagnosticsProperty('artistSpotifyUrl', artistSpotifyUrl))
+      ..add(DiagnosticsProperty('itemImgs', itemImgs))
+      ..add(DiagnosticsProperty('itemName', itemName))
+      ..add(DiagnosticsProperty('itemDescription', itemDescription))
+      ..add(DiagnosticsProperty('price', price))
+      ..add(DiagnosticsProperty('size', size))
+      ..add(DiagnosticsProperty('categories', categories));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ItemImpl &&
+            (identical(other.uid, uid) || other.uid == uid) &&
+            (identical(other.itemId, itemId) || other.itemId == itemId) &&
+            (identical(other.isSaled, isSaled) || other.isSaled == isSaled) &&
+            (identical(other.artistSpotifyId, artistSpotifyId) ||
+                other.artistSpotifyId == artistSpotifyId) &&
+            (identical(other.artistName, artistName) ||
+                other.artistName == artistName) &&
+            (identical(other.artistSpotifyUrl, artistSpotifyUrl) ||
+                other.artistSpotifyUrl == artistSpotifyUrl) &&
+            const DeepCollectionEquality().equals(other._itemImgs, _itemImgs) &&
+            (identical(other.itemName, itemName) ||
+                other.itemName == itemName) &&
+            (identical(other.itemDescription, itemDescription) ||
+                other.itemDescription == itemDescription) &&
+            (identical(other.price, price) || other.price == price) &&
+            (identical(other.size, size) || other.size == size) &&
+            (identical(other.categories, categories) ||
+                other.categories == categories));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      uid,
+      itemId,
+      isSaled,
+      artistSpotifyId,
+      artistName,
+      artistSpotifyUrl,
+      const DeepCollectionEquality().hash(_itemImgs),
+      itemName,
+      itemDescription,
+      price,
+      size,
+      categories);
+
+  /// Create a copy of Item
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ItemImplCopyWith<_$ItemImpl> get copyWith =>
+      __$$ItemImplCopyWithImpl<_$ItemImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ItemImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Item implements Item {
+  const factory _Item(
+      {required final String uid,
+      required final String itemId,
+      required final bool isSaled,
+      required final String artistSpotifyId,
+      required final String artistName,
+      required final String artistSpotifyUrl,
+      required final List<String> itemImgs,
+      required final String itemName,
+      required final String itemDescription,
+      required final int price,
+      required final String size,
+      required final String categories}) = _$ItemImpl;
+
+  factory _Item.fromJson(Map<String, dynamic> json) = _$ItemImpl.fromJson;
+
+  @override
+  String get uid;
+  @override
+  String get itemId;
+  @override
+  bool get isSaled;
+  @override
+  String get artistSpotifyId;
+  @override
+  String get artistName;
+  @override
+  String get artistSpotifyUrl;
+  @override
+  List<String> get itemImgs;
+  @override
+  String get itemName;
+  @override
+  String get itemDescription;
+  @override
+  int get price;
+  @override
+  String get size;
+  @override
+  String get categories;
+
+  /// Create a copy of Item
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$ItemImplCopyWith<_$ItemImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/data/item/item.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'item.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ItemImpl _$$ItemImplFromJson(Map<String, dynamic> json) => _$ItemImpl(
+      uid: json['uid'] as String,
+      itemId: json['itemId'] as String,
+      isSaled: json['isSaled'] as bool,
+      artistSpotifyId: json['artistSpotifyId'] as String,
+      artistName: json['artistName'] as String,
+      artistSpotifyUrl: json['artistSpotifyUrl'] as String,
+      itemImgs:
+          (json['itemImgs'] as List<dynamic>).map((e) => e as String).toList(),
+      itemName: json['itemName'] as String,
+      itemDescription: json['itemDescription'] as String,
+      price: (json['price'] as num).toInt(),
+      size: json['size'] as String,
+      categories: json['categories'] as String,
+    );
+
+Map<String, dynamic> _$$ItemImplToJson(_$ItemImpl instance) =>
+    <String, dynamic>{
+      'uid': instance.uid,
+      'itemId': instance.itemId,
+      'isSaled': instance.isSaled,
+      'artistSpotifyId': instance.artistSpotifyId,
+      'artistName': instance.artistName,
+      'artistSpotifyUrl': instance.artistSpotifyUrl,
+      'itemImgs': instance.itemImgs,
+      'itemName': instance.itemName,
+      'itemDescription': instance.itemDescription,
+      'price': instance.price,
+      'size': instance.size,
+      'categories': instance.categories,
+    };
